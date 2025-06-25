725f072c6dae5f3942c1078b1207ab68309dc9a1
HarikumarSenthil <harikumar29937@gmail.com>
2025-06-25 08:11:56 +0530

    implemented Cors

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+// server.js or index.js or app.js
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const { initializeDatabase } = require('./utils/db');
 const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true 
+}));
 
 app.use(express.json());
 app.use(cookieParser());
